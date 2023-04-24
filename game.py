a54a172e977e956f2cf3bd439be10eca8d79a217
RazvanB <braturazvan81@yahoo.ro>
2023-04-24 13:18:15 +0300

    Adding all sprites creating movment

--- game.py
+++ game.py
@@ -5,17 +5,17 @@ import random
 
 pygame.init()
 pygame.display.set_caption("Arkanoid")
-
 #Game constants:
 WIDTH,HEIGHT = 800, 600
 FPS = 60
 PLAYER_WIDTH, PLAYER_HEIGHT = 80, 20
 BRICK_WIDTH, BRICK_HEIGHT = 60, 20
 BALL_WIDTH, BALL_HEIGHT = 20,20
+PLAYER_VEL = 10
 #Game images
 BG = pygame.transform.scale(pygame.image.load(os.path.join("data", "background.jpg")), (WIDTH,HEIGHT))
 PLAYER = pygame.transform.scale(pygame.image.load(os.path.join("data", "player.png")),(PLAYER_WIDTH,PLAYER_HEIGHT))
-BRICK = pygame.transform.scale(pygame.image.load(os.path.join("data", "brick.png")),(BRICK_WIDTH, BRICK_HEIGHT))
+BRICK = pygame.transform.scale(pygame.image.load(os.path.join("data", "purple_brick.png")),(BRICK_WIDTH, BRICK_HEIGHT))
 BALL = pygame.transform.scale(pygame.image.load(os.path.join("data", "ball.png")), (BALL_WIDTH, BALL_HEIGHT))
 #Game BG and TICK rate
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -27,6 +27,7 @@ class Player:
         self.y = y
         self.lives = lives
         self.player_img = PLAYER
+        self.mask = pygame.mask.from_surface(self.player_img)
     def draw(self, window):
         window.blit(self.player_img,(self.x, self.y))
 
@@ -35,8 +36,19 @@ class Ball:
         self.x = x
         self.y = y
         self.ball_img = BALL
+        self.mask = pygame.mask.from_surface(self.ball_img)
+        self.ball_vel_x = 3
+        self.ball_vel_y = 4
     def draw(self, window):
         window.blit(self.ball_img, (self.x, self.y))
+    def update_position(self):
+        self.x += self.ball_vel_x
+        self.y += self.ball_vel_y
+        if self.x < 0 or self.x > WIDTH - BALL_WIDTH:
+            self.ball_vel_x = -self.ball_vel_x
+        if self.y < 0 or self.y > HEIGHT - BALL_HEIGHT:
+            self.ball_vel_y = -self.ball_vel_y
+
 
 class Brick:
     def __init__(self, x, y, lives = 1):
@@ -44,27 +56,27 @@ class Brick:
         self.y = y
         self.lives = lives
         self.brick_img = BRICK
+        self.mask = pygame.mask.from_surface(self.brick_img)
     
     def draw(self, window):
         window.blit(self.brick_img, (self.x, self.y))
 
 class BrickPanel:
-    def __init__(self, window_width = WIDTH, window_height = HEIGHT, brick_width=60, brick_height=20, brick_spacing=10, brick_rows=5, brick_columns=10):
-        self.window_width = window_width
-        self.window_height = window_height
+    def __init__(self, brick_width=60, brick_height=20, brick_rows=5, brick_columns=12):
         self.brick_width = brick_width
         self.brick_height = brick_height
-        self.brick_spacing = brick_spacing
         self.brick_rows = brick_rows
         self.brick_columns = brick_columns
         self.bricks = self.create_bricks()
-    
+
     def create_bricks(self):
         bricks = []
+        x_gap = 40
+        y_gap = 25
         for row in range(self.brick_rows):
             for col in range(self.brick_columns):
-                x = col * (self.brick_width + self.brick_spacing) + self.brick_spacing
-                y = row * (self.brick_height + self.brick_spacing) + self.brick_spacing
+                x = col * self.brick_width + x_gap
+                y = row * self.brick_height + y_gap
                 brick = Brick(x, y)
                 bricks.append(brick)
         return bricks
@@ -77,24 +89,29 @@ def main():
     running = True
     player = Player(300,550)
     brick_panel = BrickPanel()
-    ball = Ball(330, 530)
+    ball = Ball(400, 300)
 
     def redraw_window():
         screen.blit(BG, (0,0))
         player.draw(screen)
         brick_panel.draw(screen)
         ball.draw(screen)
-
         pygame.display.update()
         
 
     while running:
         clock.tick(FPS)
         redraw_window()
-        
+        ball.update_position()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and player.x > 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_d] and player.x < WIDTH - PLAYER_WIDTH:
+            player.x += PLAYER_VEL
         
         
 
